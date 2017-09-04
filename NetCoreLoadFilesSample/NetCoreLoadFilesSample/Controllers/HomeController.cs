e935197d787f7228faf4fae260d91653bf26b59d
infly123 <infly123@sina.com>
2017-09-04 18:30:18 +0800

    完善Excel上传

--- NetCoreLoadFilesSample/NetCoreLoadFilesSample/Controllers/HomeController.cs
+++ NetCoreLoadFilesSample/NetCoreLoadFilesSample/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.IO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +17,57 @@ namespace NetCoreLoadFilesSample.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            long size = 0;
+            foreach (var file in files)
+            {
+                //var fileName = file.FileName;
+                var fileName = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+                fileName = Path.Combine(Directory.GetCurrentDirectory(), $@"Files\{fileName}");
+                size += file.Length;
+                using (FileStream fs = System.IO.File.Create(fileName))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+            ViewBag.Message = $"{files.Count}个文件 /{size}字节上传成功!";
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(string z)
+        {
+            long size = 0;
+            var files = Request.Form.Files;
+            foreach (var file in files)
+            {
+                var filename = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+                filename = Path.Combine(Directory.GetCurrentDirectory(), $@"Files\{filename}");
+                size += file.Length;
+                using (FileStream fs = System.IO.File.Create(filename))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+
+            string message = $"上传成功了";
+            return Json(message); ;
         }
     }
 }
